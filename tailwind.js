19c078f1a0fd1ada039b48ecf63d0a5b2cfea16b
Chase Adams <c@cadams.io>
2019-05-16 11:32:06 -0700

    fix: pinned versions of emotion

--- tailwind.js
+++ tailwind.js
@@ -24,9 +24,6 @@ View the full documentation at https://tailwindcss.com.
 |
 */
 
-// eslint-disable-next-line
-const defaultConfig = require("tailwindcss/defaultConfig")();
-
 /*
 |-------------------------------------------------------------------------------
 | Colors                                    https://tailwindcss.com/docs/colors
